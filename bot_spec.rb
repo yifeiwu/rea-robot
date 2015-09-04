b5052c8587728bff7a5cc7fdafa1e9d0c4ebe764
yifeiwu <yiferic@gmail.com>
2015-09-04 17:07:44 +1200

    tentative test suite

--- bot_spec.rb
+++ bot_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Bot do
   let(:bot) { Bot.new }
   describe "#New" do
   	it "Create robot" do
-      correct_response=[nil,nil,nil]
+      correct_response = [nil,nil,nil]
       test_response = bot.position
       expect(test_response).to eq(correct_response)
     end
@@ -13,16 +13,16 @@ RSpec.describe Bot do
   describe "#Place" do
   	context 'When valid position' do
   	  it "Places the robot at designated position" do
-      	bot = Bot.place(1,1,'north')
-      	correct_response=[1,1,'north']
+      	bot.place(1,1,'north')
+      	correct_response = [1,1,'north']
         test_response = bot.position
       	expect(test_response).to eq(correct_response)
       end
     end
     context 'When invalid position' do
       it "Places the robot at designated position" do
-        bot = Bot.place(1,7,'north')
-        correct_response=[nil,nil,nil]
+        bot.place(1,7,'north')
+        correct_response = [nil,nil,nil]
         test_response = bot.position
         expect(test_response).to eq(correct_response)
       end
@@ -32,15 +32,15 @@ RSpec.describe Bot do
   describe "#Report" do
   	context 'When placed' do
       it "Report robot position" do
-        bot = Bot.place(1,1,'north')
-        correct_response=[1,1,'north']
+        bot.place(1,1,'north')
+        correct_response = [1,1,'north']
         test_response = bot.report
         expect(test_response).to eq(correct_response)
       end
     end
       context 'When not yet placed' do
       	it "Ignores command" do
-        correct_response=nil
+        correct_response = nil
         test_response = bot.report
         expect(test_response).to eq(correct_response)
       end
@@ -50,43 +50,67 @@ RSpec.describe Bot do
   describe "#Move" do
     context 'When not yet placed' do
       it "Ignores command" do
-        correct_response=nil
+        correct_response = nil
         test_response = bot.move
         expect(test_response).to eq(correct_response)
       end
     end
     context 'When placed' do
       it "Moves the robot" do
-        bot = Bot.place(1,1,'north')
+        bot.place(1,1,'north')
         bot.move
-        correct_response=[1,2,"north"]
+        correct_response = [1,2,"north"]
+        test_response = bot.report
+        expect(test_response).to eq(correct_response)
+      end
+      it "Does not move robot past table" do
+        bot.place(0,0,'south')
+        bot.move
+        correct_response = [0,0,'south']
         test_response = bot.report
         expect(test_response).to eq(correct_response)
       end
     end
   end
 
-  describe "#Turn left" do
-    it "Turns the robot left" do
-      bot = Bot.place(1,1,'north')
-      bot.left
-      correct_response=[1,1,"west"]
-      test_response = bot.position
-      expect(test_response).to eq(correct_response)
+  describe "#left" do
+    context 'When not placed' do
+      it "Ignores command" do
+        bot.left
+        correct_response = nil
+        test_response = bot.position
+        expect(test_response).to eq(correct_response)
+      end
+    end
+    context 'When placed' do
+      it "Turns the robot left" do
+        bot.place(1,1,'north')
+        bot.left
+        correct_response=[1,1,"west"]
+        test_response = bot.position
+        expect(test_response).to eq(correct_response)
+      end
     end
   end
 
-  describe "#Turn right" do
-    it "Turns the robot right" do
-      bot = Bot.place(1,1,'north')
-      bot.right
-      correct_response=[1,1,"east"]
-      test_response = bot.position
-      expect(test_response).to eq(correct_response)
+  describe "#Right" do
+    context 'When not placed' do
+      it "Ignores command" do
+      	bot.right
+        correct_response = nil
+        test_response = bot.position
+        expect(test_response).to eq(correct_response)
+      end
+    end
+    context 'When placed' do
+      it "Turns the robot right" do
+        bot.place(1,1,'north')
+        bot.right
+        correct_response=[1,1,"west"]
+        test_response = bot.position
+        expect(test_response).to eq(correct_response)
+      end
     end
   end
-
-
-
 
 end
